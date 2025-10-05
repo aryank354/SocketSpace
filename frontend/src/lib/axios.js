679a9ec67a37134e1fd10a2be266d5772e31eaf8
aryank354 <aryankanojia354@gmail.com>
2025-10-05 21:02:43 +0000

    base url changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://socketspace-xdra.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "https://socketspace-xdra.onrender.com/api"
+    : "https://socketspace-xdra.onrender.com/api", // same for production
   withCredentials: true,
 });
